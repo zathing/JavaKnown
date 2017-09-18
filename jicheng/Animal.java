0d8d3492c5066e7251b9f19db9e1d65ef7372dcb
zathing <zathing@outlook.com>
2017-09-18 11:16:04 +0800

    Create Animal.java

---
+++ jicheng/Animal.java
@@ -0,0 +1,20 @@
+package jicheng;
+
+/**
+ * Created by yldbg on 2017/9/12.
+ * 定义一个“动物”类，内部定义了“吃”、“运动”、构造方法。
+ */
+public class Animal {
+    public boolean live = true;
+    public String skin = "";
+    public void eat() {
+        System.out.println("动物需要吃食物");
+    }
+    public void move() {
+        System.out.println("动物会运动");
+    }
+    public Animal() {}
+    public Animal(String strSkin) {
+        skin = strSkin;
+    }
+}
