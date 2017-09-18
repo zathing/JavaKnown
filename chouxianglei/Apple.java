866988a7dd451c835dac510f48f9311b1c9d9554
zathing <zathing@outlook.com>
2017-09-18 11:33:19 +0800

    Update Apple.java

--- chouxianglei/Apple.java
+++ chouxianglei/Apple.java
@@ -2,7 +2,7 @@ package chouxianglei;
 
 /**
  * Created by yldbg on 2017/9/15.
- * 定义一个“苹果”类，继承Fruit抽象类并重写“收获”方法。
+ * 定义一个“苹果”类，继承Fruit抽象类并覆盖“收获”方法。
  */
 public class Apple extends Fruit {
     public void harvest() {
