c4ba1311b4a9e55c0c8edcbacdd9def80f2f39ef
zathing <zathing@outlook.com>
2017-09-18 11:05:35 +0800

    Create Fruit.java

---
+++ chouxianglei/Fruit.java
@@ -0,0 +1,13 @@
+package chouxiang;
+
+/**
+ * Created by yldbg on 2017/9/12.
+ * 定义一个抽象类（abstract），内部定义一个“收获”抽象方法。
+ */
+abstract class Fruit {
+    public String color;
+    public Fruit() {
+        color = "绿色";
+    }
+    public abstract void harvest();
+}
