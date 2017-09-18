f0bcfb934ba825289e07a46960211d302117837c
zathing <zathing@outlook.com>
2017-09-18 11:30:06 +0800

    Update Bird.java

--- jicheng/Bird.java
+++ jicheng/Bird.java
@@ -2,7 +2,7 @@ package jicheng;
 
 /**
  * Created by yldbg on 2017/9/12.
- * 定义一个继承自“动物”类的“鸟”类，重写“运动”方法。
+ * 定义一个继承自“动物”类的“鸟”类，隐藏了父类的成员变量skin，覆盖了成员方法move()。
  */
 public class Bird extends Animal {
     public String skin = "羽毛";
