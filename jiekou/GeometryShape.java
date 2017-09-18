edfce0e81cc99a587d2b241b11d3ef4a45a2b812
zathing <zathing@outlook.com>
2017-09-18 10:54:39 +0800

    Create GeometryShape.java

---
+++ jiekou/GeometryShape.java
@@ -0,0 +1,11 @@
+package jiekou;
+
+/**
+ * Created by j00406054 on 2017/9/15.
+ */
+public interface GeometryShape {
+    final float PI = 3.14159f;
+    float getArea(float r);
+    float getCircumference(float r);
+    void draw();
+}
