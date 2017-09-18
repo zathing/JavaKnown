773a92989e23562bb7312e46adffc5ade08a930f
zathing <zathing@outlook.com>
2017-09-18 11:01:46 +0800

    Update Calculate.java

--- jiekou/Calculate.java
+++ jiekou/Calculate.java
@@ -1,7 +1,8 @@
 package jiekou;
 
 /**
- * Created by j00406054 on 2017/9/15.
+ * Created by yldbg on 2017/9/15.
+ * 定义一个计算圆的秒级和周长的接口。
  */
 public interface Calculate {
     final float PI = 3.14159f;
