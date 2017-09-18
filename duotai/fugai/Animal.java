3d94c83d71859f1f1e61dfb430b17846a7453c97
zathing <zathing@outlook.com>
2017-09-18 12:10:48 +0800

    Create Animal.java

---
+++ duotai/fugai/Animal.java
@@ -0,0 +1,13 @@
+package duotai.fugai;
+
+/**
+ * Created by yldbg on 2017/9/18.
+ * 定义一个Animal类，类中生命一个成员方法cry()
+ */
+public class Animal {
+    public Animal() {}
+    public void cry() {
+        System.out.println("动物发出叫声！");
+    }
+    
+}
